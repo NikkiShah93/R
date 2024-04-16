73521d8941a3cd90a089057cd4686ead6e7c7fd7
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-04-16 18:07:04 -0400

    filtering and selecting interesting columns

--- DataWrangling/diamondData.r
+++ DataWrangling/diamondData.r
@@ -1,24 +1,28 @@
 
-## first we need to import tidyverse
+## we will be using the tidyverse family in this project
 library(tidyverse)
-
-## we'll be using diamond dataset
+## and the data we will be using is the diamond set
+## which we can quickly check to see what are the available information
 ?diamonds
-## first looking at the data
-## to get some ideas
-View(diamonds)
-## maybe we want to filter by rows
+## and take a glimpse into it as well 
+glimpse(diamonds)
+
+## lets say we only want the ideal type of diamond for our analysis
+## we can simply use filter to only get those cuts
+## and we're only interested in the ones
+## that have above average carats
 ideal_diamonds <- diamonds %>% 
-  filter(cut == 'Ideal')
+  filter(cut=='Ideal', carat >= mean(diamonds$carat) |
+           price >= mean(diamonds$price))
+## we can get the 
 glimpse(ideal_diamonds)
 
-## maybe we're interested in a few columns
-## for that we can use select
-?select
-
+## some of the columns might not be useful for our analysis
+## and we can remove them by select
 ideal_diamonds <- ideal_diamonds %>% 
-  select(carat, clarity, depth, price)
+  select(!c(x, y, z)) ## or select(!x:z)
 glimpse(ideal_diamonds)
+
 
 ## let's group them by clarity and get the average price
 avg_price_ideal_diamonds <- ideal_diamonds %>% 
