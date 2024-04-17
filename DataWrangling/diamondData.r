98b886c5444a7c6465d4d24b32d75e9bfc63f387
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-04-17 17:58:08 -0400

    rearranged the data

--- DataWrangling/diamondData.r
+++ DataWrangling/diamondData.r
@@ -11,18 +11,26 @@ glimpse(diamonds)
 ## we can simply use filter to only get those cuts
 ## and we're only interested in the ones
 ## that have above average carats
+## and we don't want any rows with nan in clarity
 ideal_diamonds <- diamonds %>% 
   filter(cut=='Ideal', carat >= mean(diamonds$carat) |
-           price >= mean(diamonds$price))
+           price >= mean(diamonds$price), !is.na(clarity))
 ## we can get the 
 glimpse(ideal_diamonds)
 
 ## some of the columns might not be useful for our analysis
 ## and we can remove them by select
 ideal_diamonds <- ideal_diamonds %>% 
-  select(!c(x, y, z)) ## or select(!x:z)
+  select(-c(x, y, z)) ## or select(!x:z) ## or select(!c(x,y,z))
 glimpse(ideal_diamonds)
 
+## lets say we want to rearrange the rows
+## in a way that the prices are in ascending order
+## where carats are in descending order
+?arrange
+ideal_diamonds <- ideal_diamonds %>% 
+  arrange(price, desc(carat))
+glimpse(ideal_diamonds)
 
 ## let's group them by clarity and get the average price
 avg_price_ideal_diamonds <- ideal_diamonds %>% 
